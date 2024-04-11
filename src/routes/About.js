93cf9a67e6f64938462bf4b28aeef6d7b9737e38
jonasskjoeth <jonas.84@live.dk>
2024-04-11 11:16:23 +0200

    About changes

--- src/routes/About.js
+++ src/routes/About.js
@@ -3,8 +3,23 @@ import React from "react";
 const About = () => {
   return (
     <>
-      <h1>About</h1>
-      <p>This is the about page</p>
+      <h1>Welcome to the ultimate companion for every Pokémon Trainer – the Pokédex! 🌟</h1>
+
+      
+<p>
+The Pokédex is your trusty electronic encyclopedia, meticulously crafted to assist you on your journey to becoming a Pokémon Master. 
+</p>
+<p>
+Whether you're a seasoned Trainer or just starting out, this invaluable tool is here to enhance your adventure and deepen your understanding of the amazing world of Pokémon.
+</p>
+<p>
+With its vast database of Pokémon species, the Pokédex is your gateway to discovering the secrets, strengths, and peculiarities of every creature you encounter. From the fiery depths of Charizard's flames to the mysterious aura of Mewtwo's psychic powers, each entry is a treasure trove of information waiting to be explored.
+
+But the Pokédex is more than just a collection of data – it's a testament to the boundless curiosity and spirit of exploration that drives Trainers to seek out new Pokémon and forge unforgettable bonds with their partners. As you journey through forests, mountains, and seas, let the Pokédex be your guide, illuminating the wonders of the Pokémon world with each new discovery.
+
+So, whether you're embarking on a quest to catch 'em all or simply looking to learn more about your favorite Pokémon, the Pokédex is here to accompany you every step of the way. Let the adventure begin!
+
+Start your journey today with the Pokédex – your key to unlocking the mysteries of the Pokémon universe!</p>
     </>
   );
 };
